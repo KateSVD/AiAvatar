f5b4cf4b466faa95125b17b86ae50db7360e8778
KateSVD <4exile.ks@gmail.com>
2024-08-30 15:17:00 +0200

    sample inputs

---
+++ SadTalker/sadtalker.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.append('C:\AI\SadTalker2')
+import SadTalker2.module_name
---
+++ app.py
@@ -0,0 +1,11 @@
+
+
+def generate_avatar():
+    print('SadTalker face is generated')
+
+def generate_audio():
+    print('TTS wav is generated')
+
+if __name__ == '__main__':
+    generate_audio()
+    generate_avatar()
