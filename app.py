1af3c21f140425cbc3e3163e8dd15681fb510cb0
o_Ojsmile <10smile97@gmail.com>
2024-09-04 17:43:20 +0200

    Added wav file generation from a text line.
    tts_models/en/ljspeech/tacotron2-DDC model is used as the most effective one for test purposes.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+from coqui_TTS.audio_generator import generate_audio_from_text
 
 
 def generate_avatar():
     print('SadTalker face is generated')
 
+
 def generate_audio():
-    print('TTS wav is generated')
+    generate_audio_from_text('TTS wav is generated', './output/audio.wav')
+
 
 if __name__ == '__main__':
     generate_audio()
---
+++ coqui_TTS/audio_generator.py
@@ -0,0 +1,10 @@
+import torch
+from TTS.api import TTS
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def generate_audio_from_text(text, audio_path):
+    tts = TTS("tts_models/en/ljspeech/tacotron2-DDC").to(device)
+    tts.tts_to_file(text, file_path=audio_path)
