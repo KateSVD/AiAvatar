4e23154dbaa25821f989162f62600f2bf24295b0
KateSVD <4exile.ks@gmail.com>
2024-09-12 22:17:04 +0200

    called SadTalker as a module

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from coqui_TTS.audio_generator import generate_audio_from_text
+#from coqui_TTS.audio_generator import generate_audio_from_text
+import sys
+import os
+sys.path.append('C:\AI\AiAvatar\SadTalker')
 
+import inference
 
 def generate_avatar():
+    os.chdir('SadTalker')
     args = inference.set_default_args()
-
-    args.source_image = "input\face1.png"
-    args.driven_audio = 'input\Thank.wav'
-    args.init_path = 'SadTalker'
-    args.enhancer = 'gfpgan'
-
-    sad_talker_inference(args)
-
+    args.driven_audio = './input/audio.wav'
+    args.source_image = 'input\Face1.png'
+    inference.sad_talker_inference(args)
 
 def generate_audio():
-    generate_audio_from_text('TTS wav is generated', './output/audio.wav')
+    generate_audio_from_text('Hello audio generator', './input/audio.wav')
 
 
 if __name__ == '__main__':
-    generate_audio()
+    #generate_audio()
     generate_avatar()
